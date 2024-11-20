7a4d08d1698ef4da00313e43873b1b7ae9bcccf9
zanereis <zanereis@pm.me>
2024-11-20 14:22:10 -0800

    Stub SMP Commands

---
+++ app/src/main/java/org/example/Commands.java
@@ -0,0 +1,124 @@
+package org.example;
+
+public class Commands {
+    public static String queueURI(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+
+    //SMP COMMANDS
+    public static String ping(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+
+    // Recipient Commands
+    public static String create(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String subscribe(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String rcvSecure(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String enableNotifications(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String disableNotifications(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String getMessage(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String acknowledge(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String suspend(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String delete(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String getQueueInfo(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+
+    // Sender Commands
+    public static String send(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String sndSecure(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+
+    // Proxy Commands
+    public static String proxySession(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String proxyCommand(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+    public static String relayCommand(/*TODO*/) {
+        // TODO
+        return ""; // TODO
+    }
+
+    // Server Messages
+    public static QueueIdResponse queueIds(String serverResponse) {
+        return null; // TODO
+    }
+    public static Object message(/*TODO*/) {
+        return null; // STUB
+    }
+    public static Object notifierId(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object messageNotification(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object proxySessionKey(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object proxyResponse(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object relayResponse(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object unsubscribed(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object queueInfo(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object ok(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+    public static Object error(/*TODO*/) {
+        // TODO
+        return null; // TODO
+    }
+}
