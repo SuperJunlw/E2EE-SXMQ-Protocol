806f3e2c2dc9cc55988aff52eff84bfff76999cc
Jun Wu <60298jw@gmail.com>
2024-12-06 17:20:05 -0800

    Client.java connected to the server

--- app/src/main/java/client/Client.java
+++ app/src/main/java/client/Client.java
@@ -4,10 +4,11 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
+
     public static void main(String[] args) {
         // proxy address
-        String hostname = "localhost";
-        int port = 1312; // Port number
+        String hostname = "smp4.simplex.im";
+        int port = 5223; // Port number
 
         try (Socket socket = new Socket(hostname, port)) {
             System.out.println("Connected to server successfully");
@@ -20,7 +21,7 @@ public class Client {
 
             // Send command to the server
             //replace with actually command?
-            String message = "command";
+            String message = "PING";
             writer.println(message);
 
             // server response
